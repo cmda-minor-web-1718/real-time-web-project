4977238d3b41af8814a83752d23fa3f9ef481bfa
Rick712 <rick.buter@gmail.com>
2018-04-17 15:39:31 +0200

    idk

--- app.js
+++ app.js
@@ -13,16 +13,20 @@ const port = 3000
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-app.get('/', function(req, res) {
-    res.render('index')
-})
-
 const Twitter = new TwitterStream(apikey, false);
 Twitter.stream('statuses/filter', {
-    track: '#psvaja'
+    follow: '2902821'
 })
 
 Twitter.pipe(fs.createWriteStream('tweets.json'));
+
+app.get('/', function (req, res) {
+    res.render('index', { tweets: tweetFile })
+})
+
+Twitter.on('connection success', function (uri) {
+    console.log('connection success', uri);
+})
 
 Twitter.on('connection success', function (uri) {
     console.log('connection success', uri);
@@ -30,24 +34,24 @@ Twitter.on('connection success', function (uri) {
 
 Twitter.on('connection aborted', function () {
     console.log('connection aborted');
-})
+});
 
 Twitter.on('connection error network', function (error) {
     console.log('connection error network', error);
-})
+});
+
+Twitter.on('connection error stall', function () {
+    console.log('connection error stall');
+});
+
+Twitter.on('connection error http', function (httpStatusCode) {
+    console.log('connection error http', httpStatusCode);
+});
 
 Twitter.on('connection error unknown', function (error) {
     console.log('connection error unknown', error);
     Twitter.close();
-})
-
-Twitter.on('connection rate limit', function (httpStatusCode) {
-    console.log('connection rate limit', httpStatusCode);
-})
-
-Twitter.on('connection error http', function (httpStatusCode) {
-    console.log('connection error http', httpStatusCode);
-})
+});
 
 app.listen(port, function () {
     console.log('server is online at port ' + port)

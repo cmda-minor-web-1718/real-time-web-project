78ff9ae4c868620e3375acf7551ab376f64b2c40
VincentKempers <vincent.m.kempers@gmail.com>
2018-04-17 15:58:48 +0200

    added google maps api with test data

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const express = require("express");
+const googleMaps = require("@google/maps");
 
 
 const app = express();
+
+
 app.set('view engine', 'ejs');
+
+var googleMapsClient = googleMaps.createClient({
+  key: "AIzaSyAVTJ9w1iE5gBXHfGkK0v9ZRKw7BIGkEjs",
+  Promise: Promise
+});
 
 
 app.use("/", (req,res) => {
-  const test = "test";
-  res.render("home", { test });
+    var response;
+  googleMapsClient.geocode(
+    {
+      address: "1600 Amphitheatre Parkway, Mountain View, CA"
+    },
+    function(err, response) {
+      if (!err) {
+        var response = response.json.results;
+        response.forEach(element => {
+            console.log(element.geometry)
+        });
+      }
+    }
+  );
+  res.render("home", { response });
 });
 
 app.listen(8080);

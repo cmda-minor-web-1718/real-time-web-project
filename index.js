b7865471ededaf97955d5bb9a1fefa64f3b049e3
VincentKempers <vincent.m.kempers@gmail.com>
2018-04-19 16:50:56 +0200

    add comments

--- index.js
+++ index.js
@@ -4,27 +4,31 @@ const dotenv = require("dotenv").config();
 const http = require('http');
 const socketIO = require('socket.io');
 
-
 const app = express();
 const server = http.Server(app);
 const io = socketIO(server);
 
+// https://scotch.io/tutorials/use-ejs-to-template-your-node-application
 app.set('view engine', 'ejs');
 
+// server files in the static folder when '/static' is requested
+app.use('/static', express.static('static'));
+
+// google create the client with Api key and show promise
 var googleMapsClient = googleMaps.createClient({
   key: dotenv.parsed.DB_PROJECT_API,
   Promise: Promise
 });
 
-
 app.use("/", (req,res) => {
-    var response;
+  var response;
   googleMapsClient.geocode({address: '1600 Amphitheatre Parkway, Mountain View, CA'})
   .asPromise()
   .then((response) => {
     var response = response.json.results;
     response.forEach(element => {
       console.log(element.address_components);
+      io.emit('location', element.address_components)
     });
   })
   .catch((err) => {
@@ -35,11 +39,21 @@ app.use("/", (req,res) => {
 
 io.on('connection', (socket) => {
   socket.on('location', (location) => {
-    console.log(location);
+    googleMapsClient.geocode({ address: `${location}` })
+      .asPromise()
+      .then((response) => {
+        var response = response.json.results;
+        response.forEach(element => {
+          console.log(element.address_components);
+          io.emit('location', element.address_components)
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   })
 })
 
 app.listen(8080, () => {
   console.log('app is running on localhost:8080, WAHOOO');
 });
-
--- static/map.js
+++ static/map.js
@@ -1,5 +1,9 @@
-var two = document.getElementById('demo');
-var button = document.getElementById('geoLoc')
+var socket = io();
+var two = document.getElementById('test');
+var button = document.getElementById('geoLoc');
+var long;
+var lang;
+console.log('vince');
 
 button.addEventListener('click', function getLocation() {
     if (navigator.geolocation) {
@@ -12,4 +16,15 @@ button.addEventListener('click', function getLocation() {
 function showPosition(position) {
     two.innerHTML = "Latitude: " + position.coords.latitude +
         "<br>Longitude: " + position.coords.longitude;
+
+    long = position.coords.longitude;
+    lang = position.coords.latitude;
+
+    var location = {
+        long: long,
+        lang: lang
+    };
+    
+    console.log(long + ' , ' + lang);
+    socket.emit('location', location);
 }

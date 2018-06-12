09e646c9a49164193f56d1c2455f8df0e1b2d213
VincentKempers <me@vncn.tk>
2018-06-12 22:09:59 +0200

    add the preview to the emit

--- static/marked.js
+++ static/marked.js
@@ -1,23 +1,35 @@
-var socket = io('http://localhost');
+var socket = io(`http://localhost:7008`);
 var input = document.getElementById('input');
 var preview = document.getElementById('content');
+var tekst = 'tekst';
 
-input.addEventListener(
-	'keyup',
-	event => {
-		preview.innerHTML = marked(input.value);
-	},
-	true
-);
-
+// location
 var thisLocation = window.location.pathname;
 while (thisLocation.charAt(0) === '/') {
 	thisLocation = thisLocation.substr(1);
 }
 
+input.addEventListener(
+	'keyup',
+	event => {
+		preview.innerHTML = marked(input.value);
+		var totalOnPage = {
+			markedTekst: preview.innerHTML,
+			roomId: thisLocation,
+		}
+		socket.on('file', function (data) {
+			socket.emit('my other event', totalOnPage);
+		});
+	},
+	true
+);
 
-socket.on('news', function(data) {
-	thisLocation = data;
-	console.log(data);
-	socket.emit('my other event', { my: data });
-});
+var totalOnPage = {
+	markedTekst: preview.innerHTML,
+	roomId: thisLocation,
+}
+
+console.log(totalOnPage)
+
+
+
